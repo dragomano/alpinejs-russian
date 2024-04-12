fadc43fc9306d80070fe8bae11c1e1638741263d
Bugo <cheetah@bk.ru>
2024-04-12 13:41:02 +0500

    Extend docs schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,12 @@ import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({
+		schema: docsSchema({
+			extend: z.object({
+				origin: z.string().url().optional(),
+			}),
+		})
+	}),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
