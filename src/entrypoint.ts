0ecfda5de173a803b7a77941b4e76d2112db52a1
Bugo <cheetah@bk.ru>
2024-04-19 22:58:06 +0500

    Add plugins/sort.mdx

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -5,10 +5,11 @@ import focus from '@alpinejs/focus';
 import intersect from '@alpinejs/intersect';
 import mask from '@alpinejs/mask';
 import morph from '@alpinejs/morph';
-import slug from 'alpinejs-slug'
-import persist from '@alpinejs/persist'
-import tash from 'alpinejs-tash'
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+import sort from "@alpinejs/sort";
+import slug from 'alpinejs-slug';
+import persist from '@alpinejs/persist';
+import tash from 'alpinejs-tash';
+import Clipboard from '@ryangjchandler/alpine-clipboard';
 import validate from "@colinaut/alpinejs-plugin-simple-validate";
 
 export default (Alpine: Alpine) => {
@@ -18,6 +19,7 @@ export default (Alpine: Alpine) => {
   Alpine.plugin(intersect);
   Alpine.plugin(mask);
   Alpine.plugin(morph);
+  Alpine.plugin(sort);
   Alpine.plugin(slug);
   Alpine.plugin(persist);
   Alpine.plugin(tash);
