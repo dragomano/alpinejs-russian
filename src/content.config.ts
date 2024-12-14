5fb2802f0adbafbbe9e7617e9a8bfa82c4d84ffc
Bugo <cheetah@bk.ru>
2024-12-14 10:24:39 +0500

    Migrate to Astro v5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,15 @@
 import { defineCollection, z } from 'astro:content';
+import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({
+		loader: docsLoader(),
 		schema: docsSchema({
 			extend: z.object({
 				origin: z.string().url().optional(),
 			}),
 		})
 	}),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+	i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 };
